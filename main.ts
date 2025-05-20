146a34db3143c9ce46e44b1d0b002daa5c003af6
Jose Castro <jcastro@intelix.biz>
2025-05-20 14:44:52 -0400

    feat: update

--- main.ts
+++ main.ts
@@ -39,27 +39,59 @@ server.tool(
       };
     }
 
+    const { latitude, longitude } = data.results[0];
 
-    const {latitude, longitude} = data.results[0]
+    const weatherResponse = await fetch(
+      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&current=precipitation,temperature_2m,relative_humidity_2m`
+    );
 
-    const weatherResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&current=precipitation,temperature_2m,relative_humidity_2m`)
-
-
-    const weatherData = await weatherResponse.json()
-
+    const weatherData = await weatherResponse.json();
 
     return {
       content: [
         {
           type: "text",
-          text: JSON.stringify(weatherData,null, 2),
+          text: JSON.stringify(weatherData, null, 2),
         },
       ],
     };
   }
 );
+server.tool(
+  "Obtener_info_ciudad", //Titulo de la herramienta
+  "Herramienta para obtener informacion de una ciudad", //Descripcion
+  {
+    city: z.string().describe("City name"),
+  }, //parametros que puede recibir la herramienta
+  async ({ city }) => {
+    //lo que queremos que haga
 
+    const response = await fetch(
+      `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=10&language=es&format=json`
+    );
 
+    const data = await response.json();
+
+    if (data.length === 0) {
+      return {
+        content: [
+          {
+            type: "text",
+            text: `No se encontro la info de ${city}`,
+          },
+        ],
+      };
+    }
+    return {
+      content: [
+        {
+          type: "text",
+          text: JSON.stringify(data, null, 2),
+        },
+      ],
+    };
+  }
+);
 
 //3. Escuchar las conexiones del cliente
 
